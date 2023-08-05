c87bd46350715bf67cdd9e85723a8611ae4b482c
indrajith <mailtoindrajith@gmail.com>
2023-08-05 11:29:14 +0530

    fourth maven project created

---
+++ src/main/java/com/rosemery/fourth_maven_project/App.java
@@ -0,0 +1,13 @@
+package com.rosemery.fourth_maven_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
